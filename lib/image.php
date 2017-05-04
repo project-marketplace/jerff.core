eb6d2751f42379803c4bc2732314fe7dfa90f4c5
TM\PopovA <a.p@team-market.ru>
2017-05-04 10:31:52 +0300

    update

--- lib/image.php
+++ lib/image.php
@@ -7,6 +7,7 @@ use CFile;
 class Image {
 
     static public function resize($ID, $width, $height) {
+        return CFile::ResizeImageGet($ID, array('width' => $width, 'height' => $height), BX_RESIZE_IMAGE_PROPORTIONAL, true)['src'];
     }
 
 }
