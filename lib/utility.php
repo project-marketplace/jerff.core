a7b45de6f7761daf2adc2e33f40e2c3615a34478
TM\PopovA <a.p@team-market.ru>
2017-06-23 10:42:47 +0300

    update

--- lib/utility.php
+++ lib/utility.php
@@ -6,6 +6,7 @@ use Bitrix\Main\Data\Cache;
 
 class Utility {
 
+    const CACHE_DAY = 86400;
     const CACHE_TIME = 3600;
     const CACHE_MINUTE = 60;
     const CACHE_DIR = '/project.core/';
@@ -21,11 +22,19 @@ class Utility {
         $cacheId = 'project.core:' . $time . ':' . (is_array($cacheId) ? implode(':', $cacheId) : $cacheId);
         $cache = Cache::createInstance();
         if ($cache->initCache($time, $cacheId, self::CACHE_DIR)) {
+//            pre('get');
             $arResult = $cache->getVars();
         } elseif ($cache->startDataCache()) {
             $arResult = $func();
+            if (empty($arResult)) {
+//                pre('abortDataCache');
+                $cache->abortDataCache();
+            } else {
+//                pre('set');
+            }
             $cache->endDataCache($arResult);
         }
+//        pre($cacheId, $arResult);
         return $arResult;
     }
 
